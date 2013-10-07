700359c2ce05fce33b25155ed7fa95817f0e31b2
Dmitry Zakharov <zaharovmag@gmail.com>
2013-10-07 11:48:45 +0400

    nvidia

--- src/nvidia.c
+++ src/nvidia.c
@@ -1,28 +1,38 @@
 #include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
 #define HAVE_NVIDIA
 #include <stat_common.h>
 
-#define  __NV_MAX_DEV 2
+#define  __NV_DEF_DEV_COUNT 2
+#define FOR_EACH(x) for(int i = 0;i < x;i++)
 
 typedef struct __device_link device_link;
+
 struct __device_link
 {
-  device_link * next;
-  nvmlDevice_t * __nv_dev;
+    device_link * next;
+    nvmlDevice_t * __nv_dev;
+    int devno;
+    unsigned int pci_gen;
 };
 
-typedef struct __device_link_head device_link_head;
-struct __device_link_head
+typedef struct __device_head device_head;
+
+struct __device_head
 {
   device_link *first;
   device_link *last;
-  device_link *arr_ptr;
+  device_link **arr_ptr;
+  unsigned int arr_len;
+  unsigned int arr_cur;
 };
 
 typedef struct __nv_ctx nv_ctx;
+
 struct __nv_ctx{
-  unsigned int device_count;
-  device_link_head * dev_head;
+  unsigned int devs;
+  device_head * dh;
 };
 
 #define __NVML_SUCCESS(x) (x == NVML_SUCCESS)
@@ -39,7 +49,76 @@ struct __nv_ctx{
 
 
 
-int fix_unsupported_bug(nvmlDevice_t device)
+static device_head *
+__alloc_dev_head()
+{
+  device_head *dh = NULL;
+
+  dh = malloc(sizeof(device_head));
+  dh->first = NULL;
+  dh->last = NULL;
+
+  dh->arr_ptr = malloc(sizeof(void *) * __NV_DEF_DEV_COUNT );
+  if(!dh->arr_ptr){
+    LOG_ERROR("malloc error\n");
+    free(dh);
+    return NULL;
+  }
+  dh->arr_len = __NV_DEF_DEV_COUNT;
+  dh->arr_cur = 0;
+
+  return dh;
+}
+
+
+static void
+__dev_add(nv_ctx * ctx,device_link *link)
+{
+  device_head * dh;
+
+  if(!ctx || !ctx->devs)
+    abort();
+
+  dh = ctx->dh;
+
+  if(!dh->first){
+    dh->first = link;
+    dh->first->next = link;
+    dh->last = link;
+    dh->arr_ptr[0] = (void *)link;
+    dh->arr_cur ++;
+  }else{
+    /*TODO extend array*/
+    dh->first->next = link;
+    dh->last = link;
+    dh->arr_ptr[dh->arr_cur] =  link;
+    dh->arr_cur++;
+  }
+}
+
+static device_link *
+__alloc_dev(int i)
+{
+    device_link *dl;
+    dl = malloc(sizeof(device_link));
+    if(!dl){
+	LOG_ERROR("alloc error\n");
+	return NULL;
+    }
+
+    dl->devno = i;
+    dl->next = NULL;
+    dl->__nv_dev = malloc(sizeof(nvmlDevice_t));
+    if(!dl->__nv_dev){
+	LOG_ERROR("alloc error\n");
+	free(dl);
+	return NULL;
+    }
+    return dl;
+}
+
+static int
+__fix_unsupported_bug(nvmlDevice_t device)
 {
     unsigned int *fix = (unsigned int *)device;
     char version[NVML_SYSTEM_NVML_VERSION_BUFFER_SIZE];
@@ -70,13 +149,30 @@ int fix_unsupported_bug(nvmlDevice_t device)
 static void _nv_init(nv_ctx * ctx)
 {
   int ret;
+  device_link *dl;
 
   ret = nvmlInit();
   NV_CK(ret,"nvmlInit()");
 
 
-  ret = nvmlDeviceGetCount(&ctx->device_count);
+  ret = nvmlDeviceGetCount(&ctx->devs);
   NV_CK(ret,"GetDeviceCOunt()\n");
+  ctx->dh = __alloc_dev_head();
+  if(!ctx->dh)
+      abort();
+
+  FOR_EACH(ctx->devs){
+      __dev_add(ctx,__alloc_dev(i));
+      dl = ctx->dh->arr_ptr[i];
+
+      ret = nvmlDeviceGetHandleByIndex(i,dl->__nv_dev);
+      NV_CK(ret,"nvmlDeviceGet");
+      ret = __fix_unsupported_bug(*dl->__nv_dev);
+
+      ret = nvmlDeviceGetCurrPcieLinkGeneration(*dl->__nv_dev,&dl->pci_gen);
+      NV_CK(ret,"getPciGen");
+
+  }
 }
 
 static void _nv_shutdown(nv_ctx *ctx)
@@ -92,7 +188,7 @@ int main()
 
   _nv_init(ctx);
 
-  printf("nvidia: dev_count: %d\n",ctx->device_count);
+  printf("nvidia: dev_count: %d\n",ctx->devs);
 
 
   _nv_shutdown(ctx);
